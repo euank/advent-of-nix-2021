d033272620ff77d885c84a4c703e21b51b29963f
Euan Kemp <euank@euank.com>
2021-12-01 13:33:46 -0800

    day1

--- day1/default.nix
+++ day1/default.nix
@@ -2,6 +2,16 @@
 with pkgs;
 let
   lines = builtins.map lib.toInt (lib.splitString "\n" (lib.fileContents ./input.lines));
-  res = lib.foldl (a: b: { prev = b; count = a.count + (if b > a.prev then 1 else 0);}) ({count = 0; prev = (lib.head lines);}) lines;
+  countIncreasing = nums:
+  let
+    x = lib.foldl (a: b: { prev = b; count = a.count + (if b > a.prev then 1 else 0);}) ({count = 0; prev = (lib.head nums);}) nums;
+  in x.count;
+
+  windowSums = lines: lib.zipListsWith(a: b: a+b) (lib.zipListsWith (a: b: a+b) lines (lib.tail lines)) (lib.tail (lib.tail lines));
+
+
 in
-  res.count
+  {
+    part1 = countIncreasing lines;
+    part2 = countIncreasing (windowSums lines);
+  }
