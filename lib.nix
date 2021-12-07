6a7d24feee26dacceb97102575d27728782cbec1
Euan Kemp <euank@euank.com>
2021-12-07 00:54:43 -0800

    lib: simplify bitset
    rec lets us reference bitSet just fine

--- lib.nix
+++ lib.nix
@@ -1,12 +1,10 @@
 { pkgs }:
 with pkgs.lib;
-{
+rec {
   # Convert a binary representation of a string to an integer
   fromBinary = str: foldl' (acc: n: acc * 2 + n) 0 (map toInt (stringToCharacters str));
 
-  bitSet =
-    let bitSet = n: i: if i == 0 then (mod n 2) == 1 else bitSet (n / 2) (i - 1);
-    in bitSet;
+  bitSet = n: i: if i == 0 then (mod n 2) == 1 else bitSet (n / 2) (i - 1);
 
   splitStringWhitespace = s: pkgs.lib.flatten (builtins.filter builtins.isList (builtins.split "([^ ]+)" s));
 
