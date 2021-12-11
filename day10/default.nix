f72a12270a9f3e84ba3687dbb941a3685b90d789
Euan Kemp <euank@euank.com>
2021-12-11 15:45:55 -0800

    day10: part2

--- day10/default.nix
+++ day10/default.nix
@@ -40,7 +40,50 @@ let
   };
 
   scoreChars = cs: foldl' builtins.add 0 (map (c: scores."${c}") cs);
+
+  # Part 2
+  # So, the root of the problem is "what completes a line"
+  # The answer really is just whatever's on the stack because of how we structured this
+  # To avoid breaking part1, copy+paste and modify
+
+  # Filter to what we need
+  incompleteChar = lines: filter (l: (findMismatchedChar l) == null) lines;
+
+  findMissingChars = lineChars:
+    let
+      f = stack: chars:
+        # No chars, just return the stack
+        if (length chars) == 0 then stack
+        else
+          let
+            c = head chars;
+            closing = isClosing c;
+          in
+          if closing && (c != (head stack)) then throw "Mismatched char, oh no"
+          else if closing then f (tail stack) (tail chars)
+          # else opening, add expected closing to the stack
+          else f ([ pairs."${c}" ] ++ stack) (tail chars);
+    in
+    f [ ] lineChars;
+
+  missingChars = lines: map findMissingChars (incompleteChar lines);
+
+  p2scores = {
+    ")" = 1;
+    "]" = 2;
+    "}" = 3;
+    ">" = 4;
+  };
+  scorePart2Line = line: foldl' (acc: c: acc * 5 + p2scores."${c}") 0 line;
+  scorePart2Chars = lineChars: map scorePart2Line lineChars;
+
+  middleScore = scores:
+    let
+      ss = sort lessThan scores;
+    in
+    elemAt ss ((length ss) / 2);
 in
 {
   part1 = scoreChars (mismatchedLineChars lines);
+  part2 = middleScore (scorePart2Chars (missingChars lines));
 }
