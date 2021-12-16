c89d71c014fba4fecce6bf5e7552a8062dd540e2
Euan Kemp <euank@euank.com>
2021-12-16 00:47:42 -0800

    day15: optimize part1 a bit
    This brings it down from 300s to 90s on my machine.
    
    Still probably too slow (since part2 needs us to do a larger graph which
    I doubt will complete at this rate), but better.

--- day15/default.nix
+++ day15/default.nix
@@ -20,61 +20,62 @@ let
   # max possible value, sum of all ints here. Used as 'infinity' for our shortest path found so far
   max = graph: foldl' builtins.add 0 (flatten graph);
 
-  getMinUnexploredPoint = graph: explored:
-    let
-      width = length (head graph);
-      height = length graph;
-      coords = cartesianProductOfSets { x = range 0 (width - 1); y = range 0 (height - 1); };
-      isUnexplored = c: (get2dArrDef explored c.x c.y 1) == 0;
-    in
-    foldl' (m: c: if m == null && (isUnexplored c) then c else if ! isUnexplored c then m else if (get2dArr graph c.x c.y) < (get2dArr graph m.x m.y) then c else m) null coords;
+  isExplored = c: explored: explored ? "${toString c.x}-${toString c.y}";
 
-  # state = { shortestPaths = [][]; explored = [][]; }
+  getMinUnexploredPoint = graph: unexplored:
+    foldl' (m: c: if (get2dArr graph c.x c.y) < (get2dArr graph m.x m.y) then c else m) (head unexplored) (tail unexplored);
+
+  # state = { shortestPaths = [][]; explored = { "${x}-${y}" = boolean; }; unexplored = []{x = int; y = int }; }
   shortestVal = point: state: graph:
     let
       height = length graph;
       width = length (head graph);
-      inherit (state) shortestPaths explored;
+      inherit (state) shortestPaths explored unexplored;
     in
     # We explored the destination, all done
-    if (get2dArr explored (width - 1) (height - 1)) == 1 then state
-    else if (get2dArr explored point.x point.y) == 1 then throw "explored node visited again"
+    if isExplored { x = width - 1; y = height - 1; } explored then state
     else
       let
         neighbors = adjacentCoords point;
         # Find points actually within the graph and not visited
-        unvistedNeighbors = filter (c: (get2dArrDef explored c.x c.y 1) == 0) neighbors;
+        unvistedNeighbors = filter (c: c.x >= 0 && c.y >= 0 && c.x < height && c.y < width && !(isExplored c explored)) neighbors;
         # Update their shortest paths
         myShortest = get2dArr shortestPaths point.x point.y;
         # Update shortestPaths for each of these points
-        shortest' = foldl' (acc: c: let
-          curShortest = get2dArr acc c.x c.y;
-          val = get2dArr graph c.x c.y;
-        in
-        if (myShortest + val) < curShortest then (set2dArr acc c.x c.y (myShortest + val)) else acc) shortestPaths unvistedNeighbors;
+        shortest' = foldl'
+          (acc: c:
+            let
+              curShortest = get2dArr acc c.x c.y;
+              val = get2dArr graph c.x c.y;
+            in
+            if (myShortest + val) < curShortest then (set2dArr acc c.x c.y (myShortest + val)) else acc)
+          shortestPaths
+          unvistedNeighbors;
         # Mark us as visited
-        explored' = set2dArr explored point.x point.y 1;
+        explored' = explored // { "${toString point.x}-${toString point.y}" = true; };
+        unexplored' = filter (c: c.x != point.x || c.y != point.y) unexplored;
         # Find the next point to visit
-        nextPoint = getMinUnexploredPoint shortest' explored';
+        nextPoint = getMinUnexploredPoint shortest' unexplored';
       in
-      shortestVal nextPoint { shortestPaths = shortest'; explored = explored'; } graph;
+      shortestVal nextPoint { shortestPaths = shortest'; explored = explored'; unexplored = unexplored'; } graph;
 
   part1Answer = filename:
     let
       graph = getData filename;
       height = length graph;
       width = length (head graph);
+      coords = cartesianProductOfSets { x = range 0 (width - 1); y = range 0 (height - 1); };
       initShortest =
         let
           m = max graph;
           infGraph = map (l: map (_: m) l) graph;
         in
         set2dArr infGraph 0 0 0;
-      initExplored = map (l: map (_: 0) l) graph;
+      initExplored = { };
 
       state = shortestVal
         ({ x = 0; y = 0; })
-        ({ shortestPaths = initShortest; explored = initExplored; })
+        ({ shortestPaths = initShortest; explored = initExplored; unexplored = coords; })
         graph;
     in
     get2dArr state.shortestPaths (width - 1) (height - 1);
